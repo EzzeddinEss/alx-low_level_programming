e33fdf91f2816214855201638569b045195c3a11
EzzeddinEss <ej_dozze95@outlook.com>
2022-12-11 14:34:04 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include<stdio.h>
 /*
-* print line of code
-* return 0
+ * print line of code
+ *
+ * return 0
 */
-
-int main()
+int main(void)
 {
-   
 	char cIsFun[] = "\"Programming is like building a multilingual puzzle";
-
 	puts(cIsFun);
-
 	return (0);
 }
