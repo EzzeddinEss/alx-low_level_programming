34f5fe1fd24d8ba24576d10c2f6dad45b2c0fe02
EzzeddinEss <ej_dozze95@outlook.com>
2022-12-11 14:24:07 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-#!/bin/bash
 #include<stdio.h>
+/*
+* print line of code
+* return 0
+*/
+
 int main()
 {
-
-	char cIsFun[] = "Programming is like building a multilingual puzzle";
+   
+	char cIsFun[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(cIsFun);
 
-	return 0;
+	return (0);
 }
