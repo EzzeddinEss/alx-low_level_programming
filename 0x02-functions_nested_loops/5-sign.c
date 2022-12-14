65ad8c345e6158ba85df8435c70b725ef1046bd6
EzzeddinEss <ej_dozze95@outlook.com>
2022-12-14 14:26:59 -0800

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - check if the number is equal to 1 or 0 or -1.
+ *
+ * Return: 1 if Greater than Zero.
+ * 0 if equal Zero.
+ * -1 if less than Zero
+ *
+ * @n: is the checking parameter
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+}
