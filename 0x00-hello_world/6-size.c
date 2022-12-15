6d75a33c257c103a4a4f4b90c189f9ec62d529f8
EzzeddinEss <ej_dozze95@outlook.com>
2022-12-15 13:37:16 -0800

    The numbers speak for themselves

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+ * main - Type the size of data
+ *
+ *Return: 0
+*/
+
+int main(void)
+{
+	char c;
+	int x;
+	long int y';
+        long long int j;
+	floaf f;
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers -  a function that prints the num, from 0 to 9.
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		_putchar("%s\n", i);
+	}
+}
