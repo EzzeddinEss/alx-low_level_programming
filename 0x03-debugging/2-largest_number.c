41cd44e9793f03221f5a628cb155180a5bf051c1
EzzeddinEss <ej_dozze95@outlook.com>
2022-12-16 18:23:40 -0800

    debug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,22 @@ int largest_number(int a, int b, int c)
 
 	if (a > b)
 	{
-		if (b > c)
+		if (a > c)
+		{
 			largest = a;
-		else if (a > c)
-			largest = a;
+		}
 		else
+		{
 			largest = c;
+		}
+	}
+	else if (b > c)
+	{
+		largest = b;
 	}
 	else
 	{
-		if (b < c)
-			largest = c;
-		else
-			largest = b;
+		largest = c;
 	}
 	return (largest);
 
