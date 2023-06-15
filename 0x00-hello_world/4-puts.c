632e962092766b6797ca934dae6c8f3700350352
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-06-15 11:30:16 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Print a text using puts function only
+ *
+ * Return: Alwayes 0
+ */
+
+int main(void)
+{
+  char strg[100] = "Programming is like building a multilingual puzzle";
+  puts(strg);
+  return (0);
+}
