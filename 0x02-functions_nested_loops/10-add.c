24563338ef75655553f2eb8d395495dd858dee86
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-06-20 13:51:09 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * add - a function that adds two integers and returns the result
- * @n1: the value of the first number.
- * @n2: the value of the second number.
+ * @num1: the value of the first number.
+ * @num2: the value of the second number.
  *
  * Return: The result of adding these two numbers.
  */
 
-int add(int n1, int n2)
+int add(int num1, int num2)
 {
 int result;
-result = n1 + n2;
+result = num1 + num2;
 return (result);
 }
