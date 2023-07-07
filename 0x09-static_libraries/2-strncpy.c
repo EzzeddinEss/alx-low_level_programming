173d5306824772aff5fad49a2625f8685e85ea26
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-07-07 08:41:13 -0700

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,5 +1,9 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+
 
 char *_strncpy(char *dest, char *src, int n)
 {
+return strncpy(dest, src, n);
 }
--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,5 +1,9 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+
 
 int _strcmp(char *s1, char *s2)
 {
+return strcmp(s1, s2);
 }
