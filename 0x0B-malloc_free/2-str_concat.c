07dba27943a12aed62f4f83c7fa6cc5d01646abf
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-07-11 10:18:17 -0700

    It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - a function that concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: a pointer to new Alloc memory space, null on failure.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+}
