03bac2d2467af68e3b5280c44e6755d05c8201a8
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-07-13 12:57:50 -0700

    a function that creates an array of integers.

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - a function that allocates memory for an array, using malloc
+ * @nmemb: the nubmer of bytes to be allocated
+ * @size: the size of memory to be allocated
+ *
+ * Return: 0 if nmemb or size return null, and if it fails.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+  unsigned int *ptr;
+  
+}
