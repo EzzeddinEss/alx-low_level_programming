338d52098a53727499e276f2fa2d8bfba2cc37c2
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-07-13 13:10:59 -0700

    a function that creates an array of integers.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,11 +21,11 @@ while (s2[c2] != '\0')
 c2++;
 if (n < c2)
 {
-ptr = malloc(sizeof(ptr) * (c1 + n + 1));
+ptr = malloc(sizeof(char) * (c1 + n + 1));
 }
 else
 {
-ptr = malloc(sizeof(ptr) * (c1 + c2 + 1));
+ptr = malloc(sizeof(char) * (c1 + c2 + 1));
 }
 if (ptr == NULL)
 {
