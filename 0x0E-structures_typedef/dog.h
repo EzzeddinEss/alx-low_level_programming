bd6a5a65f09009d008eac7b8de3aa3932c61c4ad
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-07-17 12:49:23 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,12 +17,7 @@ float age;
 char *owner;
 }
 
-/**
- * dog_t - typedef for dog struct
- */
-
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
