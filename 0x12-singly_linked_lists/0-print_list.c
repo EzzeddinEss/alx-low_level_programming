ccda42b9ec9f21b83e621beb3958795e944090ec
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-07-27 10:11:34 -0700

    code fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,14 @@
 
 size_t print_list(const list_t *h)
 {
-size_t stl = 0;
+int stl = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
 {
 printf("[%d] %s \n", 0, "(nil)");
 }
-else if
+else
 {
 printf("[%d] %s \n", h->len, h->str);
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,17 +1,7 @@
 #include "lists.h"
-
-/**
- * _strlen - returns the length of a string
- * @s: pointer to a string
- * Return: length of string
- */
-int _strlen(const char *s)
-{
-int i = 0;
-while (s[i] != '\0')
-i++;
-return (i);
-}
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * add_node - adds a new node at the beginning og the list.
@@ -24,14 +14,18 @@ return (i);
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *newNode;
-newNode = malloc(sizeof(list_t));
+int count = 0;
 
+while (str[count])
+count++;
+
+newNode = malloc(sizeof(list_t));
 if (newNode == NULL)
 {
 return (NULL);
 }
 newNode->str = strdup(str);
-newNode->len = _strlen(str);
+newNode->len = count;
 newNode->next = *head;
 *head = newNode;
 
