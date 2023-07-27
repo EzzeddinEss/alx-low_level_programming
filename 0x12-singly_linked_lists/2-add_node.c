71528d5cd0e612697886583f18854b75907d50c4
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-07-27 11:53:34 -0700

    Code Fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,12 +18,17 @@ int count;
 for (count = 0; str[count];)
 count++;
 newNode = malloc(sizeof(list_t));
+if(newNode == NULL)
+return (NULL);
 if (str != NULL)
 {
 newNode->str = strdup(str);
+if (!newNode->str)
+{
+free(newNode);
+return (NULL);
+}
 newNode->len = count;
-if (newNode->str == NULL)
-return(NULL);
 }
 
 newNode->next = *head;
