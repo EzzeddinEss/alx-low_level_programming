8913c0b8a00924cf2fcc3f160b6893c991397ad8
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-08-03 11:24:29 -0700

    function that returns the value of a bit at a given index.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary -  function prints the binary representation of a number.
+ * @n: the number to print in the representation.
+ *
+ * Return: Nothing.
+ */
+
+void print_binary(unsigned long int n)
+{
+}
