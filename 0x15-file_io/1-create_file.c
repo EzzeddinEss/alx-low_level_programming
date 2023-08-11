06880af55ded56f27136cac2f315dac3428fb089
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-08-11 00:08:06 -0700

    code fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 int fd, _write, len = _strlen(text_content);
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 if (len != '\0')
 {
 _write = write(fd, text_content, len);
