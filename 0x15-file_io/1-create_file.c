f2fe9dc3b79d8946aff2fea30b2806cc9cda008a
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-08-11 10:58:34 -0700

    code fix 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,6 @@
  * @s: pointer to a string
  * Return: length of string
  */
-
 int _strlen(const char *s)
 {
 int i = 0;
@@ -18,28 +17,22 @@ return (i);
  * create_file - function that creates a file.
  * @filename: the name of the file to be created.
  * @text_content: the content of the file.
- *
  * Return: incase of fail -1 otherwise 1;
  */
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-ssize_t _write = 0, len = 0;
+ssize_t _write = 0, len = _strlen(text_content);
 if (filename == NULL)
 return (-1);
 fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
-if (text_content != NULL)
-{
-len = _strlen(text_content);
+if (len != '\0')
 _write = write(fd, text_content, len);
-if (_write == -1)
-{
 close(fd);
-return (-1);
-}
-}
-close(fd);
+if (_write != 1)
+return (1);
+else
 return (-1);
 }
