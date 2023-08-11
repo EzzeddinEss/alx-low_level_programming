3c4fa236a6f07147d99d2060324fb5548c5a9194
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-08-11 00:39:26 -0700

    code fix 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,15 +25,13 @@ int create_file(const char *filename, char *text_content)
 {
 int fd;
 ssize_t _write, len = _strlen(text_content);
-if (filename == 0)
+if (filename == 0 || fd == -1)
 return (-1);
 fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
-if (len == '\0')
+if (len != '\0')
 {
 _write = write(fd, text_content, len);
 }
-if (fd == -1)
-return (-1);
 if (_write == -1)
 return (-1);
 else
