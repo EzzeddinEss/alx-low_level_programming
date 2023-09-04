743adba0fe0fef8cb2d6ada6e9fabca423a7a848
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-09-04 04:58:52 -0700

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to a string
+ * Return: length of string
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	i++;
+	return (i);
+}
+
+/**
+ * create_file - function that creates a file.
+ * @filename: the name of the file to be created.
+ * @text_content: the content of the file.
+ * Return: incase of fail -1 otherwise 1;
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t _written = 0, len = _strlen(text_content);
+
+	if (filename == NULL)
+	return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL && len != 0)
+	{
+		_written = write(fd, text_content, len);
+		if (_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
+
