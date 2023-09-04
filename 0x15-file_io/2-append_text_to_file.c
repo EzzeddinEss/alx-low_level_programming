20b916cfa0181fcd9c9a6c172e4e8252af71daa3
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-09-04 06:41:20 -0700

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to a string
+ * Return: length of string
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	i++;
+	return (i);
+}
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: the name of the file.
+ * @text_content: the content to append.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t _wirtten = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (len != 0)
+	{
+		_wirtten = write(fd, text_content, len);
+	}
+	close(fd);
+	return (_wirtten == len ? 1 : -1);
+}
+
