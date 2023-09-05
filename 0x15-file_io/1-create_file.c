0415282746957e2547e9d3efce33bc5b4afce45f
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-09-05 02:15:19 -0700

    code fix 4

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 	return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permissions & ~S_IXUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permissions);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL && len)
